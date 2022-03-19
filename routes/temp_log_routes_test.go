6ca32f680cb88c1d0d453f5365955ae98ab71c8a
wellington gadelha <contato.informeai@gmail.com>
2022-03-19 11:19:19 -0300

    feat(routes/temp_logs_routes__test.go): create file for tests in routes

---
+++ routes/temp_log_routes_test.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"testing"
+)
+
+//go test -run ^TestHealth
+func TestHealth(t *testing.T) {
+	r := NewRouter()
+	if err := r.health(); err != nil {
+		t.Errorf("TestHealth: got: %s -> expect: == nil\n", err.Error())
+	}
+}
+
+//go test -run ^TestGetLogs
+func TestGetLogs(t *testing.T) {
+	r := NewRouter()
+	if err := r.getLogs(); err != nil {
+		t.Errorf("TestGetLogs: got: %s -> expect: == nil\n", err.Error())
+	}
+}
+
+//go test -run ^TestPostLogs
+func TestPostLogs(t *testing.T) {
+	r := NewRouter()
+	if err := r.postLogs(); err != nil {
+		t.Errorf("TestPostLogs: got: %s -> expect: == nil\n", err.Error())
+	}
+}
