b509cce16f960dbe48798ca8838a4e605917620d
wellington gadelha <contato.informeai@gmail.com>
2022-03-19 11:20:42 -0300

    feat(services/post_logs.go): create service for post log

---
+++ services/post_logs.go
@@ -0,0 +1,29 @@
+package services
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/informeai/temp-log/dto"
+)
+
+//TransformLog is struct for transform logs
+type TransformLog struct {
+	Message string
+}
+
+//NewTransformLog return new instance of TransformLog
+func NewTransformLog() *TransformLog {
+	return &TransformLog{}
+}
+
+//Transform is return log transformed
+func (t TransformLog) Transform(log *dto.Log) (TransformLog, error) {
+	if len(log.Type) < 3 || len(log.Message) < 5 {
+		return TransformLog{}, errors.New("length of type or message not permited")
+	}
+	t.Message = fmt.Sprintf("[%v]:[%s]: %s", time.Now().UTC().Format(time.RFC3339), strings.ToUpper(log.Type), log.Message)
+	return t, nil
+}
